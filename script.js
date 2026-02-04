2dc6c738e155b158c1412443a17cbd7b358452c2
cyako <243948749+matin-cyako@users.noreply.github.com>
2026-02-05 00:09:13 +0900

    ランダムで花言葉が出現する機能追加

--- script.js
+++ script.js
@@ -53,3 +53,38 @@ function loadTodo() {
         todos.forEach(todo => createTodoItem(todo.text, todo.done));
     }
 };
+
+const flowerFortune = [
+    { name: "ブルースター", meaning: "「信じあう心」「幸福な愛」" },
+    { name: "ネモフィラ", meaning: "「どこでも成功」「可憐」" },
+    { name: "青いカーネーション", meaning: "「永遠の幸福」" },
+    { name: "アジサイ（青）", meaning: "「辛抱強い愛情」「知的」" },
+    { name: "ムスカリ", meaning: "「明るい未来」「寛大な愛」" },
+    { name: "デルフィニウム", meaning: "「清明」「あなたは幸福をふりまく」" },
+    { name: "ミモザ", meaning: "「感謝」「友情」「優雅」" },
+    { name: "ヒマワリ", meaning: "「あなただけを見つめる」「憧れ」" },
+    { name: "黄色いガーベラ", meaning: "「究極の愛」「親しみやすい」" },
+    { name: "フリージア（黄）", meaning: "「無邪気」「期待」" },
+    { name: "バラ（黄）", meaning: "「友情」「献身」" },
+    { name: "菜の花", meaning: "「快活」「明るさ」" },
+    { name: "カスミソウ", meaning: "「感謝」「無邪気」「幸福」" },
+    { name: "スズラン", meaning: "「再び幸せが訪れる」「純粋」" },
+    { name: "白いバラ", meaning: "「純潔」「深い尊敬」" },
+    { name: "ガーデニア", meaning: "「私は幸せ者」「とても幸せ」" },
+    { name: "デイジー（白）", meaning: "「希望」「無邪気」" },
+    { name: "ジャスミン", meaning: "「愛想の良い」「優美」" },
+    { name: "青いバラ", meaning: "「夢かなう」「奇跡」「不可能を可能にする」" },
+    { name: "グロリオサ（黄）", meaning: "「栄光」「勇敢」" },
+];
+
+function showFlower() {
+    const randomIdx = Math.floor(Math.random() * flowerFortune.length);
+    const todayFlower = flowerFortune[randomIdx];
+
+    document.getElementById('flower-name').textContent = todayFlower.name;
+    document.getElementById('flower-meaning').textContent = todayFlower.meaning;
+}
+
+window.onload = function() {
+    showFlower();
+};
